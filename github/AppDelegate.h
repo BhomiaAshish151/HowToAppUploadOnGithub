df7ff1807d7aabbcb81599be275dd3d622fc3ffd
your-nameAshish <your-emailashish.bhomia@gmail.com>
2015-01-09 20:26:37 +0530

    first commit

---
+++ github/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  github
+//
+//  Created by NOTOITSOLUTIONS on 09/01/15.
+//  Copyright (c) 2015 NOTO SOLUTIONS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
